377dd41b71e1c464909bc0e1eeeff467157e1432
Suriyaa Sundararuban <github@suriyaa.tk>
2024-05-12 14:36:38 +0200

    Create setup.py

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Elara Aerospace - Rocket Flight Simulation
-> This repository contains the simulation program for Elara Aerospace's rocket tests.
+> Python-powered trajectory simulation for Elara Aerospace Rocketry
+
+This repository contains the simulation program for Elara Aerospace's rocket tests.
 
 ## License
-(c) 2024 Suriyaa Sundararuban
 (c) 2024 Elara Aerospace team
---
+++ setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+necessary_require = [
+    "rocketpy",
+    "numpy>=1.13",
+    "scipy>=1.0",
+    "matplotlib>=3.0",
+    "netCDF4>=1.6.4",
+    "requests",
+    "pytz",
+    "simplekml",
+]
+
+env_analysis_require = [
+    "timezonefinder",
+    "windrose>=1.6.8",
+    "IPython",
+    "ipywidgets>=7.6.3",
+    "jsonpickle",
+]
+
+setuptools.setup(
+    name="elara-simulations",
+    version="1.0.0",
+    install_requires=necessary_require,
+    extras_require={
+        "env_analysis": env_analysis_require,
+        "all": necessary_require + env_analysis_require,
+    },
+    maintainer="Elara Aerospace team",
+    author="Suriyaa Sundararuban",
+    author_email="github@suriyaa.tk",
+    description="Python-powered trajectory simulation for Elara Aerospace Rocketry.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.comelara-aerospace/rocket-simulations",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8",
+)
